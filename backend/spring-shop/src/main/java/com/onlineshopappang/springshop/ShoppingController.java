598249f706ad1c5bdba7b64fc32354068b176b64
David Bilauca <david.bilauca@gmail.com>
2026-01-13 17:04:41 +0200

    -

--- backend/spring-shop/src/main/java/com/onlineshopappang/springshop/ShoppingController.java
+++ backend/spring-shop/src/main/java/com/onlineshopappang/springshop/ShoppingController.java
@@ -1,4 +1,22 @@
 package com.onlineshopappang.springshop;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
 public class ShoppingController {
+
+    @Value("${app.somevar}")
+    private String var;
+
+    @RequestMapping("/")
+    public String index(){
+        System.out.println("somevar: "+var);
+        return "index.html";
+    }
+
+    private String[][] getAllProducts(){
+        return new String[][]{new String[]{"a"}, new String[]{"b"}};
+    }
 }
